80a1b566fbf17f6e5588a68c956ea16fd47310b7
Xero kun <elo48858@gmail.com>
2023-06-17 20:54:05 +0100

    `FEAT` Implemented linked list data structure

--- src/main/kotlin/datastructures/LinkedList.kt
+++ src/main/kotlin/datastructures/LinkedList.kt
@@ -1,6 +1,31 @@
 package datastructures
 
-class LinkedList<T>(
-    val item: T,
-    val next: LinkedList<T>
-)
+private class Node<T>(val item: T) {
+    var next: Node<T>? = null
+}
+
+class LinkedList<T> {
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+
+    fun addNode(item: T) {
+        val newNode = Node(item)
+
+        if (head == null) {
+            head = newNode
+            tail = newNode
+        } else {
+            tail?.next = newNode
+            tail = newNode
+        }
+    }
+
+    fun iterateList() {
+        var curr = head
+
+        while (curr != null) {
+            println(curr.item)
+            curr = curr.next
+        }
+    }
+}
---
+++ src/test/kotlin/testdatastructures/TestLinkedList.kt
@@ -0,0 +1,8 @@
+package testdatastructures
+
+import datastructures.LinkedList
+
+class TestLinkedList {
+    val head = LinkedList<Int>(1)
+    head.next = LinkedList<Int>(2)
+}
