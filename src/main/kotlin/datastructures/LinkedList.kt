6d127a800809e0437aa8367dad78ac856f179032
Xero kun <elo48858@gmail.com>
2023-06-20 11:53:35 +0100

    `FEAT` Defined accessor and mutator methods for private properties

--- src/main/kotlin/datastructures/LinkedList.kt
+++ src/main/kotlin/datastructures/LinkedList.kt
@@ -9,5 +9,17 @@ class Node<T>(val item: T) {
 }
 
 class LinkedList<T> {
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+    private var listSize: Int = 0
+
+    val size: Int
+        get() = listSize
+
+    val headNode: Node<T>?
+        get() = head
+
+    val tailNode: Node<T>?
+        get() = tail
 
 }
