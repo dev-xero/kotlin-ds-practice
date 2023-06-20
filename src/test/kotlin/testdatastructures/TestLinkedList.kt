2680b67dcffc0b24c80b0ad1117c4b55f65d1f07
Xero kun <elo48858@gmail.com>
2023-06-20 12:48:42 +0100

    `TESTS` Tested addToHead and addToTail methods

--- src/test/kotlin/testdatastructures/TestLinkedList.kt
+++ src/test/kotlin/testdatastructures/TestLinkedList.kt
@@ -27,4 +27,21 @@ class TestLinkedList {
 
         assertFalse(linkedList.isEmpty)
     }
+
+    @Test
+    fun testListSizeUpdatesCorrectlyAfterAddingToHead() {
+        linkedList.addToHead(1)
+        linkedList.addToHead(2)
+
+        assertTrue(linkedList.size == 2)
+    }
+
+    @Test
+    fun testLinkedListSizeUpdatesCorrectlyAfterAddingToTail() {
+        linkedList.addToTail(1)
+        linkedList.addToTail(2)
+        linkedList.addToTail(3)
+
+        assertTrue(linkedList.size == 3)
+    }
 }
